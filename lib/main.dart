855033db67617d979f1506212126de9d85461955
jimmyff <jimmy@rocketware.co.uk>
2021-11-11 19:43:07 +0000

    Updated examples for new chroma

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 
 class ImagePaletteScreen extends StatefulWidget {
   final exampleImageURLs = [
-    // Landscape pics
+    // // Landscape pics
     'https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&h=512&w=512',
     'https://images.pexels.com/photos/567540/pexels-photo-567540.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=512&w=512',
     'https://images.pexels.com/photos/460775/pexels-photo-460775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=512&w=512',
@@ -47,13 +47,14 @@ class ImagePaletteScreen extends StatefulWidget {
 
   static String get title => 'Lorem ipsum dolor sit';
   static String get para1 =>
-      'Pellentesque euismod, arcu auctor dapibus lacinia, ex mauris elementum arcu, consequat euismod urna arcu at ex. Nulla ut est eu risus ultricies tempor. Curabitur ut semper mi. ';
+      'Pellentesque euismod, arcu auctor dapibus lacinia, ex mauris elementum arcu. ';
   static String get para2 =>
       'Donec varius, diam a malesuada tincidunt, ante sem gravida lectus, scelerisque scelerisque nulla augue quis ligula. Quisque porttitor sapien nunc, a malesuada justo ornare et. Nullam vel elementum dui.';
 }
 
 class _ImagePaletteScreenState extends State<ImagePaletteScreen> {
-  Map<List<int>, UiTheme> palettes = {};
+  Map<Uint8List, List<List<ColorRgb>>> palettes = {};
+  bool loading = true;
 
   @override
   void initState() {
@@ -73,11 +74,16 @@ class _ImagePaletteScreenState extends State<ImagePaletteScreen> {
         print('HTTP GET $url #${response.statusCode}');
         if (response.statusCode == 200) {
           setState(() {
-            palettes[response.bodyBytes] = UiTheme.fromJpg(response.bodyBytes);
+            palettes[response.bodyBytes] =
+                UiTheme.fromJpg(response.bodyBytes, 5);
           });
         } else
           print('Failed to download image $url #${response.statusCode}');
       }));
+    });
+    await Future.wait(futures);
+    setState(() {
+      loading = false;
     });
   }
 
@@ -89,205 +95,67 @@ class _ImagePaletteScreenState extends State<ImagePaletteScreen> {
           title: Text('Image Palette Examples'),
           backgroundColor: Colors.transparent,
         ),
-        body: PageView(
-          children: palettes.keys.map<Widget>((image) {
-            final theme = palettes[image]!;
-            var bgColor = Color(theme.dark.toInt());
-            var fgColor = Color(theme.light.toInt());
+        body: loading
+            ? Container(
+                child: Center(
+                child: CircularProgressIndicator.adaptive(),
+              ))
+            : PageView(
+                children: palettes.keys.map<Widget>((image) {
+                  final theme = palettes[image]!;
 
-            return Container(
-                color: bgColor,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        child: Image.memory(Uint8List.fromList(image)),
-                        onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    SingleImagePaletteDebug(
-                                      jpgBytes: image,
-                                    ))),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                          ImagePaletteScreen.title,
-                          style: TextStyle(
-                              color: fgColor,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold),
+                  return Container(
+                      child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Image.memory(
+                          Uint8List.fromList(image),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        child: Text(
-                          ImagePaletteScreen.para1,
-                          style: TextStyle(color: fgColor, fontSize: 18),
-                        ),
-                      ),
-                      Container(
-                          padding: EdgeInsets.all(16),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              ElevatedButton.icon(
-                                  style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Color(theme.primary.toInt()))),
-                                  onPressed: () {},
-                                  icon: Icon(Icons.star),
-                                  label: Text('Primary')),
-                              ElevatedButton.icon(
-                                  style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Color(theme.secondary.toInt()))),
-                                  onPressed: () {},
-                                  icon: Icon(Icons.favorite),
-                                  label: Text('Secondary')),
-                            ],
-                          )),
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        child: Card(
-                            color: Color(theme.light.toInt()),
-                            child: Container(
-                              padding: EdgeInsets.all(16),
-                              child: Text(ImagePaletteScreen.para2,
-                                  style: TextStyle(
-                                    color: Color(theme.dark.toInt()),
-                                    fontSize: 18,
-                                  )),
-                            )),
-                      )
-                    ],
-                  ),
-                ));
-          }).toList(),
-        ));
-  }
-}
-
-class SingleImagePaletteDebug extends StatefulWidget {
-  final List<int> jpgBytes;
-
-  const SingleImagePaletteDebug({Key? key, required this.jpgBytes})
-      : super(key: key);
-  @override
-  _SingleImagePaletteDebugState createState() =>
-      _SingleImagePaletteDebugState();
-}
-
-class _SingleImagePaletteDebugState extends State<SingleImagePaletteDebug> {
-  late UiTheme swatch;
-  Map<Image, UiTheme> palettes = {};
-  @override
-  void initState() {
-    super.initState();
-    _process();
-  }
-
-  Future _process() async {
-    setState(() {
-      swatch = UiTheme.fromJpg(widget.jpgBytes, debug: true);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var bgColor = Color(swatch.dark.toInt());
-    var fgColor = Color(swatch.light.toInt());
-
-    return Scaffold(
-        backgroundColor: Color(swatch.dark.toInt()),
-        appBar: AppBar(
-          title: Text('Single Image Palette Debug'),
-          backgroundColor: Color(swatch.primary.toInt()),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _process,
-          child: Icon(Icons.undo),
-          backgroundColor: Color(swatch.primary.toInt()),
-        ),
-        body: swatch == null
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Image.memory(Uint8List.fromList(widget.jpgBytes)),
-                    Container(
-                      padding: EdgeInsets.only(top: 24, bottom: 8),
-                      child: Center(
-                          child: Text(
-                        ImagePaletteScreen.title,
-                        style: TextStyle(
-                            color: fgColor,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold),
-                      )),
+                        ...theme.map<Widget>((List<ColorRgb> theme) {
+//
+                          final primary = Color(theme[0].toInt());
+                          final secondary = Color(theme[1].toInt());
+                          return Container(
+                              color: primary,
+                              child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(16),
+                                      child: Text(
+                                        ImagePaletteScreen.title,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(16),
+                                      child: Text(
+                                        ImagePaletteScreen.para1,
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(16),
+                                      child: ElevatedButton.icon(
+                                          style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all(
+                                                      secondary)),
+                                          onPressed: () {},
+                                          icon: Icon(Icons.favorite),
+                                          label: Text('Secondary')),
+                                    ),
+                                  ]));
+                        }),
+                      ],
                     ),
-                    Container(
-                        padding: EdgeInsets.all(16),
-                        child: Center(
-                          child: Text(ImagePaletteScreen.para1,
-                              style: TextStyle(
-                                color: fgColor,
-                                fontSize: 16,
-                              )),
-                        )),
-                    Container(
-                        padding: EdgeInsets.all(16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            ElevatedButton.icon(
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(
-                                        Color(swatch.primary.toInt()))),
-                                onPressed: () {},
-                                icon: Icon(Icons.star),
-                                label: Text('Primary')),
-                            ElevatedButton.icon(
-                                style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all(
-                                        Color(swatch.secondary.toInt()))),
-                                onPressed: () {},
-                                icon: Icon(Icons.favorite),
-                                label: Text('Secondary')),
-                          ],
-                        )),
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      child: Card(
-                          color: Color(swatch.light.toInt()),
-                          child: Container(
-                            padding: EdgeInsets.all(16),
-                            child: Text(ImagePaletteScreen.para2,
-                                style: TextStyle(
-                                  color: Color(swatch.dark.toInt()),
-                                  fontSize: 16,
-                                )),
-                          )),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      child: Card(
-                          color: Colors.black,
-                          child: Container(
-                            padding: EdgeInsets.all(16),
-                            child: Text(swatch.toHexArray().join(', '),
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                )),
-                          )),
-                    ),
-                  ],
-                ),
+                  ));
+                }).toList(),
               ));
   }
 }
